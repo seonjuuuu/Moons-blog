ec52326f52bdb3c80ce6cefa96a012123dab5103
MoonSsunny <moonsun116@naver.com>
2024-03-27 18:27:25 +0900

    블로그이름 변경

--- site.config.js
+++ site.config.js
@@ -18,8 +18,8 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "M.Seonju Develop Blog",
+    description: "welcome to my blog!",
   },
 
   // CONFIG configration (required)
