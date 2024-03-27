b1a23fffd8fffeb8d3092325844aebf835467243
MoonSsunny <moonsun116@naver.com>
2024-03-27 12:47:40 +0900

    profile 변경

--- site.config.js
+++ site.config.js
@@ -1,14 +1,14 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    name: "M.seonju",
+    image: "/avatar.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    bio: "프론트엔드 개발 ...ing",
+    email: "moonsun116@naver.com",
+    linkedin: "-",
+    github: "https://github.com/MoonSsunny",
+    instagram: "-",
   },
   projects: [
     {
