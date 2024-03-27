f17037351549b7fbf9ee782c8f9c0d5346adc6d7
MoonSsunny <moonsun116@naver.com>
2024-03-27 22:58:50 +0900

    수정

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "M.seonju",
-    image: "/avatar.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "프론트엔드 개발 ...ing",
     email: "moonsun116@naver.com",
