5b6d7a27e1c48e7b515d8c9f8d9e069ea019d25d
MoonSsunny <moonsun116@naver.com>
2024-03-27 19:59:46 +0900

    변경test

--- site.config.js
+++ site.config.js
@@ -18,7 +18,7 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "M.Seonju Develop Blog",
+    title: "M.Seonju Develop Blog 👋",
     description: "welcome to my blog 👋",
   },
 
